4c3f864223c3dc093eaee99731d5d787d2f5339d
sanja <sanja@LAPTOP-PDD355CV>
2017-02-27 17:57:33 -0600

    Methods/functions declared

--- CalcAPP/CalcAPP/MainPage.xaml.cs
+++ CalcAPP/CalcAPP/MainPage.xaml.cs
@@ -13,5 +13,21 @@ namespace CalcAPP
         {
             InitializeComponent();
         }
+        //call the OnSelected Method/Functiom
+        void OnSelectNumber(object sender, EventArgs args)
+        {
+        }
+        //call the on onSlectOperator Method/Functiom
+        void OnSelectOperator(object sender, EventArgs args)
+        {
+        }
+        //call the on OnClear Method/Functiom
+        void OnClear(object sender, EventArgs args)
+        {
+        }
+        // call the on OnCalculate Method/Functiom
+        void OnCalculate(object sender, EventArgs args)
+        {
+        }
     }
 }
